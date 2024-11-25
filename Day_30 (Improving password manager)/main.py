db23e2f2627fb0e134aa4ca2accfae44e46abb3d
Akash kewat <kewatakash42@gmail.com>
2024-11-25 11:24:31 +0530

    Add files via upload

---
+++ Day_30 (Improving password manager)/main.py
@@ -0,0 +1,16 @@
+# file not found
+# with open("a_file.txt")as file:
+#     file.read()
+
+# key error
+# a_dictionary = {"key" : "value"}
+# value = a_dictionary["not_present_key"]
+
+# index error
+# fruit_list = ["apple", "mango", "vana"]
+# fruit_list[5]
+
+# type error
+# text = "abc"
+# print(text + 5)
+
